a4335358d133173d3db963b022190d4f0aa07ac5
Sathvik Jammula <BL.EN.U4AIE20020@bl.students.amrita.edu>
2022-11-09 16:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,7 @@ import streamlit as st
 from datetime import datetime
 import datetime
 from statsmodels.tsa.arima.model import ARIMA
-st.session_state['answer'] = '!'
 
-st.write(st.session_state)
-
-realans = ['', 'abc', 'edf']
-
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
-
-st.write(st.session_state)
-st.write(answerStat)
 
 # Reading all the files from the given input directory
 dirPath = r"/Users/sathvikjammula/PycharmProjects/Hackathon/Currency_Conversion_Test_Data"
